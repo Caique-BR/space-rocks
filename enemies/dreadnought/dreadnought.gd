09ff8d6dcc27430c43b3b8e2771edd4b574960bc
Caique-BR <caique.scotti@gmail.com>
2026-02-03 19:35:31 -0300

    no

--- enemies/dreadnought/dreadnought.gd
+++ enemies/dreadnought/dreadnought.gd
@@ -2,13 +2,33 @@ class_name Dreadnought
 extends AnimatableBody2D
 
 @export var health = 3
-@export var checkpoints : Array[Vector2] ## 0 = spawn, 1 enters screen, 2 = top left, 3 bot left, 4 top right, 5 bot right
+@export var checkpoints : Array[Vector2] ## 0 = enters screen, 1 = top left, 2 bot left, 3 top right, 4 bot right
 var speed = 100
+var current_pos : Vector2 = global_position
+var tween_move : Tween 
+
 
 func _ready() -> void:
-	pass
+	var next_position: Vector2 = checkpoints[0]
+	move_to_spawn(next_position)
 
-func _physics_process(delta: float) -> void:
-	var next_position : Vector2 = checkpoints[3]
-	look_at(next_position)
-	position += transform.x * (speed * delta)
+
+	
+func move_to_spawn(to: Vector2): # boss arrives from outside the top center of the screen and stop a top mid of screen
+	
+		look_at(to)
+		if tween_move: tween_move.kill()
+		tween_move = create_tween()
+		tween_move.tween_property(self, "position", to, 1)
+		
+		tween_move.finished.connect(move_to_top_left)
+		
+		
+		
+func move_to_top_left(): 
+		print("finished")
+		look_at(checkpoints[1])
+		if tween_move: tween_move.kill()
+		tween_move = create_tween()
+		tween_move.tween_property(self, "position", checkpoints[1], 1)
+		
--- main/main.gd
+++ main/main.gd
@@ -114,5 +114,5 @@ func _on_enemy_timer_timeout(): # enemy spawn
 
 func _on_dreadnought_timertest_timeout() -> void:
 	var e = dreadnought_scene.instantiate()
-	e.position = Vector2(950, 200)
-	#add_child(e)
+	e.position = Vector2(950, -200)
+	add_child(e)
