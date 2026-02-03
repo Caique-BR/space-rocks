8e4e3fea03628616bdcfcd1959d1e26f8763d215
reinaldorm <105465734+reinaldorm@users.noreply.github.com>
2026-02-03 10:00:40 -0300

    Implement movement towards checkpoint in dreadnought
    straight from cookies' house, toma jack 
    (provavelmente vai dar erro)

--- enemies/dreadnought/dreadnought.gd
+++ enemies/dreadnought/dreadnought.gd
@@ -4,18 +4,14 @@ extends AnimatableBody2D
 @export var health = 3
 @export var checkpoints : Array[Vector2] ## 0 = spawn, 1 enters screen, 2 = top left, 3 bot left, 4 top right, 5 bot right
 
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	rotation = PI / 2
-	
-	
-	
-	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	#follow.progress += speed * delta
 	#position = follow.global_position
-	pass
+	var next_position : Vector2 = checkpoints[3]
+	look_at(next_position)
+	move_toward(position, next_position, delta)
