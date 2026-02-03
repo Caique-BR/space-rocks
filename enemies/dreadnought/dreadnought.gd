e3d2af89e09d42992e51a02ebe09b9d8563221b1
Caique-BR <caique.scotti@gmail.com>
2026-02-03 17:57:32 -0300

    sim

--- enemies/dreadnought/dreadnought.gd
+++ enemies/dreadnought/dreadnought.gd
@@ -28,4 +28,5 @@ func _physics_process(delta: float) -> void:
 	#position = follow.global_position
 	var next_position : Vector2 = checkpoints[3]
 	look_at(next_position)
+	print(get_global_mouse_position())
 	position = position.move_toward(next_position,delta * speed)
