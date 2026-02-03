9ecea9c1b7a40ad8602eb19374624fdfc835ad91
Caique-BR <caique.scotti@gmail.com>
2026-02-03 12:30:18 -0300

    try and fail

--- enemies/dreadnought/dreadnought.gd
+++ enemies/dreadnought/dreadnought.gd
@@ -3,15 +3,29 @@ extends AnimatableBody2D
 
 @export var health = 3
 @export var checkpoints : Array[Vector2] ## 0 = spawn, 1 enters screen, 2 = top left, 3 bot left, 4 top right, 5 bot right
+var speed = 100
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	rotation = PI / 2
-
+	#rotation = PI / 2
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	pass
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	#follow.progress += speed * delta
 	#position = follow.global_position
 	var next_position : Vector2 = checkpoints[3]
 	look_at(next_position)
-	move_toward(position, next_position, delta)
+	position = position.move_toward(next_position,delta * speed)
--- main/main.gd
+++ main/main.gd
@@ -115,4 +115,4 @@ func _on_enemy_timer_timeout(): # enemy spawn
 func _on_dreadnought_timertest_timeout() -> void:
 	var e = dreadnought_scene.instantiate()
 	e.position = Vector2(950, 200)
-	#add_child(e)
+	add_child(e)
